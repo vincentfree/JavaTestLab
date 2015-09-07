128f8b5b20a6cde86a4bd8b7877704d34a5c76bd
vincentfree <vinsterdamus@gmail.com>
2015-09-07 10:05:20 +0200

    Opdracht 4, init

--- src/main/java/opmaak/Format.java
+++
@@ -1,8 +0,0 @@
-import java.util.Date;
-class Format {
-  public static void main (String args[]) {
-    System.out.format("De prijs van %1$s is %2$f Euro", "een paar schoenen", 120f);
-    System.out.println();
-    System.out.format("Het is vandaag %1$tD.", new Date());
-  }
-}
